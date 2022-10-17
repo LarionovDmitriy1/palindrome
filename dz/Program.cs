87b226568ccc472ceb8e54c4fe251a697a21d4d3
Дима Ларионов <dima_larionov_000@mail.ru>
2022-10-17 21:19:29 +0300

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите слово палиндром");
+string a=Console.ReadLine();
+int count = 0;
+string[] b= a.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+for (int i = 0; i < b.Length; i++)
+{
+    if (b[i] == new string(b[i].Reverse().ToArray()))
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество палиндромов - {count}");
+
+
+
+
